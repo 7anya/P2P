33b6e5fbe0ac4ff298a2b456f0296f2ead56d268
Tanya Prasad <f20181054@hyderabad.bits-pilani.ac.in>
2021-03-31 12:05:48 +0530

    minor changes made

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-
-import 'package:device_info/device_info.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_nearby_connections/flutter_nearby_connections.dart';
 import 'package:fluttertoast/fluttertoast.dart';
